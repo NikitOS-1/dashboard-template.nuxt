29b79815eb510b94a047f7476e542ccd600807f2
nikita <NikitaPuzyrenko93@gmail.com>
2025-04-01 12:14:42 +0300

    chore(deps): add deps and setup configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,35 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui-pro',
+    '@vueuse/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  routeRules: {
+    '/api/**': {
+      cors: true
+    }
+  },
+
+  future: {
+    compatibilityVersion: 4
+  },
+
+  compatibilityDate: '2024-07-11',
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
